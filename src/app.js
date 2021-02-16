f46aca7736a36b37d47075ae9db744b50ae57aa8
keithfilegar <keith.filegar@gmail.com>
2021-02-16 12:14:10 -0800

    add articles-service.js, seed script

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.get('/articles', (req, res, next) => {
+    res.send('All articles')
+})
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
+const knex = require('knex')
 const app = require('./app')
-const { PORT } = require('./config')
+const { PORT, DB_URL } = require('./config')
+
+const db = knex({
+    client: 'pg',
+    connection: DB_URL,
+})
 
 app.listen(PORT, () => {
     console.log(`server listening at http://localhost:${PORT}`)
