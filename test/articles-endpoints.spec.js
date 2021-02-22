056ed1fa1275e2cd49870185184eb460339cbcd7
keithfilegar <keith.filegar@gmail.com>
2021-02-22 10:16:48 -0800

    add /api prefix to backend endpoints

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -21,11 +21,11 @@ describe('Articles Endpoints', function() {
 
     afterEach('cleanup', () => db('blogful_articles').truncate())
 
-    describe(`GET /articles`, () => {
+    describe(`GET /api/articles`, () => {
         context(`Given no articles`, () => {
             it(`responds with a 200 and an empty list`, () => {
                 return supertest(app)
-                    .get('/articles')
+                    .get('/api/articles')
                     .expect(200, [])
             })
         })
@@ -41,7 +41,7 @@ describe('Articles Endpoints', function() {
 
             it('responds with 200 and all of the articles', () => {
                 return supertest(app)
-                .get('/articles')
+                .get('/api/articles')
                 .expect(200, testArticles)
             })
         })
@@ -57,7 +57,7 @@ describe('Articles Endpoints', function() {
 
             it('removes XSS attack content', () => {
                 return supertest(app)
-                    .get(`/articles/${maliciousArticle.id}`)
+                    .get(`/api/articles/${maliciousArticle.id}`)
                     .expect(200)
                     .expect(res => {
                         expect(res.body.title).to.eql(expectedArticle.title)
@@ -67,12 +67,12 @@ describe('Articles Endpoints', function() {
         })
     })
 
-    describe(`GET /articles/:article_id`, () => {
+    describe(`GET /api/articles/:article_id`, () => {
         context(`Given no articles`, () => {
             it(`responds with 404`, () => {
                 const articleId = 123456
                 return supertest(app)
-                    .get(`/articles/${articleId}`)
+                    .get(`/api/articles/${articleId}`)
                     .expect(404, { error: { message: `Article doesn't exist` } })
             })
         })
@@ -90,7 +90,7 @@ describe('Articles Endpoints', function() {
                 const articleId = 2
                 const expectedArticle = testArticles[articleId - 1]
                 return supertest(app)
-                .get(`/articles/${articleId}`)
+                .get(`/api/articles/${articleId}`)
                 .expect(200, expectedArticle)
             })
         })
@@ -106,7 +106,7 @@ describe('Articles Endpoints', function() {
 
             it('removes XSS attack content', () => {
                 return supertest(app)
-                    .get(`/articles/${maliciousArticle.id}`)
+                    .get(`/api/articles/${maliciousArticle.id}`)
                     .expect(200)
                     .expect(res => {
                         expect(res.body.title).to.eql(expectedArticle.title)
@@ -116,7 +116,7 @@ describe('Articles Endpoints', function() {
         })
     })
 
-    describe(`POST /articles`, () => {
+    describe(`POST /api/articles`, () => {
         it(`creates an article, responding with 201 and new article`, function() {
             this.retries(3)
             const newArticle = {
@@ -125,7 +125,7 @@ describe('Articles Endpoints', function() {
                 content: 'Test article new content...'
             }
             return supertest(app)
-                .post('/articles')
+                .post('/api/articles')
                 .send(newArticle)
                 .expect(201)
                 .expect(res => {
@@ -133,17 +133,16 @@ describe('Articles Endpoints', function() {
                     expect(res.body.style).to.eql(newArticle.style)
                     expect(res.body.content).to.eql(newArticle.content)
                     expect(res.body).to.have.property('id')
-                    expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+                    expect(res.headers.location).to.eql(`/api/articles/${res.body.id}`)
                     const expected = new Date().toLocaleString()
                     const actual = new Date(res.body.date_published).toLocaleString()
                     expect(actual).to.eql(expected)
                 })
                 .then(postRes =>
                     supertest(app)
-                    .get(`/articles/${postRes.body.id}`)
+                    .get(`/api/articles/${postRes.body.id}`)
                     .expect(postRes.body)
-                    )
-               
+                )
         })
 
         const requiredFields = ['title', 'style', 'content']
@@ -159,7 +158,7 @@ describe('Articles Endpoints', function() {
                 delete newArticle[field]
 
                 return supertest(app)
-                    .post('/articles')
+                    .post('/api/articles')
                     .send(newArticle)
                     .expect(400, {
                         error: { message: `Missing '${field}' in request body`}
@@ -170,7 +169,7 @@ describe('Articles Endpoints', function() {
         it('removes XSS attack content from response', () => {
             const { maliciousArticle, expectedArticle } = makeMaliciousArticle()
             return supertest(app)
-                .post(`/articles`)
+                .post(`/api/articles`)
                 .send(maliciousArticle)
                 .expect(201)
                 .expect(res => {
@@ -180,12 +179,12 @@ describe('Articles Endpoints', function() {
         })
     })
 
-    describe(`DELETE /articles/:articleId`, () => {
+    describe(`DELETE /api/articles/:articleId`, () => {
         context('Given no articles in db', () => {
             it(`responds with a 404`, () => {
                 const articleId = 123456
                 return supertest(app)
-                    .delete(`/articles/${articleId}`)
+                    .delete(`/api/articles/${articleId}`)
                     .expect(404, { error: { message: `Article doesn't exist` } })
             })
         })
@@ -203,11 +202,11 @@ describe('Articles Endpoints', function() {
                 const idToRemove = 2
                 const expectedArticles = testArticles.filter(article => article.id !== idToRemove)
                 return supertest(app)
-                    .delete(`/articles/${idToRemove}`)
+                    .delete(`/api/articles/${idToRemove}`)
                     .expect(204)
                     .then(res => 
                         supertest(app)
-                        .get('/articles')
+                        .get('/api/articles')
                         .expect(expectedArticles)
                     )
             })
